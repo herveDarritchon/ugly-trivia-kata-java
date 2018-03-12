ceb3bf8e3b36479af06011ce289296d09e89d2aa
Hervé Darritchon <herve.darritchon@gmail.com>
2018-03-12 12:07:17 +0100

    Evolution: Intercept the System.out method to assert the result of the program.

--- src/test/java/com/adaptionsoft/games/trivia/GameTest.java
+++ src/test/java/com/adaptionsoft/games/trivia/GameTest.java
@@ -1,9 +1,9 @@
 package com.adaptionsoft.games.trivia;
 
-import static org.junit.Assert.*;
+import static junit.framework.Assert.assertEquals;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import org.junit.Test;
 
@@ -11,19 +11,34 @@ import com.adaptionsoft.games.uglytrivia.Game;
 
 public class GameTest {
 
-	@Test
-	public void true_is_true() {
-		assertTrue(true);
-	}
+    private String SOME_PLAYER;
 
     @Test
-    public void whenANewGameSomethingHappens() {
+    public void whenANewGameNothingHappens() {
         // given
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
         // when
-        Game game = new Game();
+        new Game();
 
         // then
-        //assertTrue(console.getMessages().isEmpty());
+        assertEquals("", outputStream.toString());
     }
 
+    @Test
+    public void whenAPlayerIsAddedToAGameNameAndNumberOfPlayerIsDisplayed() {
+        // given
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        // when
+        Game game = new Game();
+        SOME_PLAYER = "Robert";
+        game.add(SOME_PLAYER);
+        // then
+        assertEquals("Robert was added\n"
+                + "They are player number 1\n", outputStream.toString());
+
+    }
 }
